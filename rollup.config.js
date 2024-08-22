dac2496d58506c32a3e6d07a61aebe0455f6f928
RitaTeelaunch <rita@teelaunch.com>
2024-08-22 15:09:09 +0300

    normal form

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 
 export default [{
     input: 'src/index.ts',
@@ -18,6 +19,13 @@ export default [{
         typescript({
             tsconfig: './tsconfig.json',
 }),
+        postcss({
+            modules: true, // Enables CSS modules
+            extract: true, // Extracts CSS into a separate file
+            minimize: true,
+            sourceMap: true,
+            namedExports: true, // Allows you to use named exports for classes
+        }),
     ],
     external: ['react', 'react-dom'],
 },{
